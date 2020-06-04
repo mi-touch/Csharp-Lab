e67e85bb37b2e13918f806dee96613b66a4a35ff
Timileyin Adefare <61581959+mi-touch@users.noreply.github.com>
2020-06-04 11:58:40 +0100

    Add files via upload

---
+++ Timilehin's Calculator/Timilehin's Calculator/Calculator.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Timilehin_s_Calculator
+{
+    class Calculator
+    {
+        public static double DoOperation(double num1, double num2, string op)
+        {
+            double result = double.NaN; // set default value of result to Not a number
+            switch (op)
+            {
+                case "a":
+                    result = num1 + num2;
+                    break;
+                case "s":
+                    result = num1 - num2;
+                    break;
+                case "m":
+                    result = num1 * num2;
+                    break;
+                case "d":
+                    // Ask the user to enter a non-zero divisor.
+                    if (num2 != 0)
+                    {
+                        result = num1 / num2;
+                    }
+                    break;
+                default:
+                    Console.WriteLine("Enter correct math operation. ");
+                    break;
+            }
+            return result;
+        }
+    }
+}
